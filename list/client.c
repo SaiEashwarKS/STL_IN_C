b4b6da122d4cc21890f2d04afb0aad2249d1ae2b
Sreyans Bothra <sreyansrocks@gmail.com>
2021-05-06 14:48:51 +0530

    ainvayi

--- list/client.c
+++ list/client.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 
 define_list(int);
+define_list(double);
 
 void display(list(int)* list)
 {
@@ -16,8 +17,9 @@ void display(list(int)* list)
 
 int main()
 {
-    list(int)* a;
+    list(int)* a;list(double)* b;
     a = new_list(int, 3, 10);
+    b= new_list(double, 3, 10);
     //printf("front: %d size: %d\n", front(a), size(a));
     display(a);
     push_front(a, 20);
