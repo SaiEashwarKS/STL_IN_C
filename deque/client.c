7c1a238edc936edc3ae0489af9b185c6684ff371
Sreyans Bothra <sreyansrocks@gmail.com>
2021-05-07 09:49:22 +0530

    Linked list implementation of deque

---
+++ deque/client.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "deque.h"
+// #include "list.h"
+
+define_deque(int);
+//define_list(int);
+
+/*
+void printkaro(deque_int* a)//this is for the array type implementation
+{
+	//printf("\nFRONT: %d SIZE:%d BACK:%d\n",a->front,a->size,a->back);
+	int i=a->front;
+	printf("\n");
+	for(int j=0;j<a->size;++j)
+	{printf("%d, ",a->queue[(i+j)%a->maxie]);
+	printf("\n");
+}
+*/
+void printkaro(deque_int* a)
+{
+	//printf("\nFRONT: %d SIZE:%d BACK:%d\n",a->front,a->size,a->back);
+	deque_node_int* i=a->front;
+	printf("\n");
+	for(int j=0;j<a->size;++j)
+	{printf("%d, ",i->data);i=i->next;}
+	printf("\n");
+}
+int main()
+{
+	deque(int)* a;//list(int)* b;
+	a=new_deque(int,5,10);
+	//b=new_list(int,5,10);
+	printf("\nNON EMPTY: %d\n",empty(a));
+	printf("\nSIZE: %d\n",size(a));
+	printf("\nMAX SIZE: %d\n",max_size(a));
+	printf("\nAT: %d\n",at(a,4));
+	printf("\nFRONT: %d\n",front(a));
+	insert(a,11);
+	printf("\nAFTER SIZE: %d\n",size(a));
+	printf("\nBACK: %d\n",back(a));
+	//remove(a);
+	pop(a);
+	printf("\nSIZE: %d\n",size(a));
+	printf("\nBACK: %d\n",back(a));
+	clear(a);
+	//printkaro(b);
+	printf("\nSIZE: %d\n",size(a));
+	printf("HELLO WORLD\n");
+}
+
