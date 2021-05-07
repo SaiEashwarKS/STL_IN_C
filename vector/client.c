fb96e111e87065f823467b8d4eb280cb19787ac6
Monisha Chandra <monishachandra30@gmail.com>
2021-05-07 22:39:59 +0530

    iterators (buggy)

--- vector/client.c
+++ vector/client.c
@@ -2,15 +2,112 @@
 #include<stdio.h>
 
 define_vector(int);
+define_vector(char);
+
+void display_int(vector(int) *vector)
+{
+    for(int i=0; i< vector->size; ++i)
+    {
+        printf("%d ",vector->data[i]);
+    }
+    printf("\n");
+}
+
+void display_char(vector(char) *vector)
+{
+    for(int i=0; i< vector->size; ++i)
+    {
+        printf("%c ",vector->data[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
-    vector(int)* a;
-    a = new_vector(int, 3, 10);
-    //printf("front: %d size: %d\n", front(a), size(a));
-    for(int i=0; i< a->size; ++i)
-        printf("%d ",a->data[i]);
+    printf("\ndefault ctor\n");
+    vector(char) *c = new_vector(char, 0); //default ctor
+    push_back(c, 'a');
+    push_back(c, 'b');
+    display_char(c);
+    printf("\ncopy ctor\n");
+    vector(char) *c2 = new_vector(char, c); //copy ctor
+    display_char(c2);
+
+    //delete
+    printf("\ndelete\n");
+    delete (c);
+    if (c == NULL)
+        printf("c is null\n");
+
+    printf("\nfill ctor\n");
+    vector(int) * a;
+    a = new_vector(int, 2, 10); //fill ctor
+    display_int(a);
+
+    //push
+    printf("\npush\n");
     push_back(a, 30);
-    for(int i=0; i< a->size; ++i)
-        printf("%d ",a->data[i]);
+    push_back(a, 40);
+    display_int(a);
+
+    //pop
+    printf("\npop\n");
+    display_int(a);
+    pop_back(a);
+    display_int(a);
+
+    //insert
+    //printf("\ninsert\n");
+    //insert(a, iter_list_forward(begin(a)), 1, 20);
+    //display_int(a);
+
+    //at
+    printf("\nfind\n");
+    display_int(a);
+    printf("Find 50: %d\n", find(a, 50));
+
+    /*//erase
+    printf("\nerase\n");
+    printf("before removing : ");
+    display_int(a);
+    remove_list(a, 10);
+    remove_list(a, 200);
+    printf("after removing 10 and 200 : ");
+    display_int(a);*/
+
+    //at
+    printf("\nat\n");
+    display_int(a);
+    printf("Elelment at position 2: %d\n", at(a, 2));
+
+    //size
+    printf("\nsize\n");
+    display_int(a);
+    printf("Size of a: %d\n", size(a));
+
+    //size
+    printf("\ncapacity\n");
+    printf("capacity of a: %d\n", capacity(a));
+
+    //at
+    printf("\nat\n");
+    display_int(a);
+    printf("Elelment at position 2: %d\n", at(a, 2));
+
+    //front&back
+    printf("\nfront & back\n");
+    display_int(a);
+    printf("front of a: %d\nback of a: %d\n", front(a), back(a));
+
+    //forward iterator
+    push_back(a, 40);
+    printf("\nforward iterator\n");
+    iterator_vector(int) *it = begin(a);
+    printf("forward iteration\n");
+    while (iter_list_notequal(it, end(a)))
+    {
+        printf("%d ", *it);
+        iter_list_forward(it);
+    }
+    printf("\n");
 }
--- vector/vector.h
+++ vector/vector.h
@@ -8,6 +8,7 @@
     \
     struct vector_##type; \
     struct vector_functions_pointers_##type; \
+    struct iterator_vector_##type; \
     \
     struct vector_##type {\
             int size; \
@@ -17,11 +18,27 @@
         }; \
     typedef struct vector_##type vector_##type; \
     \
+    struct iterator_vector_##type {\
+        int is_reverse; \
+        type* iter; \
+    }; \
+    typedef struct iterator_vector_##type iterator_vector_##type; \
+    \
     struct vector_functions_pointers_##type {\
         int (*empty)(const vector_##type* vector); \
         int (*size)(const vector_##type* vector); \
         int (*capacity)(const vector_##type* vector); \
         void (*push_back)(vector_##type* vector, type data); \
+        void (*pop_back)(vector_##type* vector); \
+        type (*at)(vector_##type* vector, int pos); \
+        type (*front)(vector_##type* vector); \
+        type (*back)(vector_##type* vector); \
+        int (*find)(vector_##type* vector, type data); \
+        type* (*begin)(vector_##type* vector); \
+        type* (*end)(vector_##type* vector); \
+        type* (*rbegin)(vector_##type* vector); \
+        type* (*rend)(vector_##type* vector); \
+        void (*delete)(vector_##type** vector); \
     }; \
     typedef struct vector_functions_pointers_##type vector_functions_pointers_##type; \
     \
@@ -30,13 +47,33 @@
     int size_##type (const vector_##type* vector); \
     int capacity_##type (const vector_##type* vector); \
     void push_back_##type(vector_##type* vector, type data); \
+    void pop_back_##type(vector_##type* vector); \
+    type at_##type(vector_##type* vector, int pos); \
+    type front_##type(vector_##type* vector); \
+    type back_##type(vector_##type* vector); \
+    int find_##type(vector_##type* vector, type data); \
+    type* begin_##type(vector_##type* vector); \
+    type* end_##type(vector_##type* vector); \
+    type* rbegin_##type(vector_##type* vector); \
+    type* rend_##type(vector_##type* vector); \
+    void delete_##type(vector_##type** vector); \
     \
     \
      vector_functions_pointers_##type vector_functions_##type = {\
         &empty_##type, \
         &size_##type, \
         &capacity_##type, \
-        &push_back_##type \
+        &push_back_##type, \
+        &pop_back_##type, \
+        &at_##type, \
+        &front_##type, \
+        &back_##type, \
+        &find_##type, \
+        &begin_##type, \
+        &end_##type, \
+        &rbegin_##type, \
+        &rend_##type, \
+        &delete_##type \
     }; \
     \
     \
@@ -77,6 +114,75 @@
         ++vector->size; \
     } \
     \
+    void pop_back_##type(vector_##type* vector) \
+    { \
+        --vector->size; \
+    } \
+    \
+    type at_##type(vector_##type* vector, int pos) \
+    { \
+        return vector->data[pos]; \
+    } \
+    \
+    type front_##type(vector_##type* vector) \
+    { \
+        return vector->data[0]; \
+    } \
+    \
+    type back_##type(vector_##type* vector) \
+    { \
+        return vector->data[vector->size -1]; \
+    } \
+    \
+    int find_##type(vector_##type* vector, type data) \
+    { \
+        for(int i=0; i<vector->size; ++i) \
+        { \
+            if(vector->data[i] == data) \
+                return i; \
+        } \
+        return vector->size; \
+    } \
+    \
+    type* begin_##type(vector_##type* vector) \
+    {\
+        iterator_vector_##type* it = (iterator_vector_##type*)malloc(sizeof(iterator_vector_##type)); \
+        it->is_reverse = 0; \
+        it->iter = vector->data; \
+        return it->iter; \
+    } \
+    type* end_##type(vector_##type* vector) \
+    {\
+        iterator_vector_##type* it = (iterator_vector_##type*)malloc(sizeof(iterator_vector_##type)); \
+        it->is_reverse = 0; \
+        it->iter = vector->data+vector->size; \
+        return it->iter; \
+    } \
+    \
+    type* rbegin_##type(vector_##type* vector) \
+    {\
+        iterator_vector_##type* it = (iterator_vector_##type*)malloc(sizeof(iterator_vector_##type)); \
+        it->is_reverse = 1; \
+        it->iter = NULL; \
+        return it->iter; \
+    } \
+    type* rend_##type(vector_##type* vector) \
+    {\
+        iterator_vector_##type* it = (iterator_vector_##type*)malloc(sizeof(iterator_vector_##type)); \
+        it->is_reverse = 1; \
+        it->iter = NULL; \
+        return it->iter; \
+    } \
+    \
+    void delete_##type(vector_##type** vector) \
+    {\
+        if((*vector)->capacity != 0) \
+        {\
+            free((*vector)->data); \
+        } \
+        free(*vector); \
+        *vector = NULL; \
+    } \
     \
     vector_##type* new_vector_##type(int n, ...) \
     {\
@@ -115,21 +221,94 @@
         vector_##type* vector = new_vector_default_##type(); \
         for(int i=0; i < rhs->size; ++i) \
         { \
-            vector->data[i++] = rhs->data[i++]; \
+            push_back_##type(vector, rhs->data[i]); \
         } \
-        vector->size = rhs->size; \
-        vector->capacity = rhs->capacity; \
         return vector; \
     } \
 
 #define vector(type) vector_##type
-//#define new_vector(type, n, ...) new_vector_##type(n, ##__VA_ARGS__) 
 #define new_vector(type, arg1, ...) _Generic ((arg1), \
                                             int : new_vector_##type, \
                                             vector_##type* : new_vector_copy_##type) (arg1, ##__VA_ARGS__)
-#define empty(vector) vector->functions->empty(vector)
-#define size(vector) vector->functions->size(vector)
-#define capacity(vector) vector->functions->capacity(vector)
-#define push_back(vector, data) vector->functions->push_back(vector, data)
+
+#ifndef DELETE_FUNC
+#define DELETE_FUNC
+#define delete(vector) (vector)->functions->delete(&vector)
+#endif
+
+#ifndef SIZE_FUNC
+#define SIZE_FUNC
+#define size(vector) (vector)->functions->size(vector)
+#endif
+
+#ifndef EMPTY_FUNC
+#define EMPTY_FUNC
+#define empty(vector) (vector)->functions->empty(vector)
+#endif
+
+#ifndef CAPACITY_FUNC
+#define CAPACITY_FUNC
+#define capacity(vector) (vector)->functions->capacity(vector)
+#endif
+
+#ifndef PUSH_BACK_FUNC
+#define PUSH_BACK_FUNC
+#define push_back(vector, data) (vector)->functions->push_back(vector, data)
+#endif
+
+#ifndef POP_BACK_FUNC
+#define POP_BACK_FUNC
+#define pop_back(vector) (vector)->functions->pop_back(vector)
+#endif
+
+#ifndef AT_FUNC
+#define AT_FUNC
+#define at(vector, pos) (vector)->functions->at(vector, pos)
+#endif
+
+#ifndef FRONT_FUNC
+#define FRONT_FUNC
+#define front(vector) (vector)->functions->front(vector)
+#endif
+
+#ifndef BACK_FUNC
+#define BACK_FUNC
+#define back(vector) (vector)->functions->back(vector)
+#endif
+
+#ifndef FIND_FUNC
+#define FIND_FUNC
+#define find(vector, data) (vector)->functions->find(vector, data)
+#endif
+
+#ifndef BEGIN_FUNC
+#define BEGIN_FUNC
+#define begin(vector) (vector)->functions->begin(vector)
+#endif
+
+#ifndef END_FUNC
+#define END_FUNC
+#define end(vector) (vector)->functions->end(vector)
+#endif
+
+#ifndef RBEGIN_FUNC
+#define RBEGIN_FUNC
+#define rbegin(vector) (vector)->functions->rbegin(vector)
+#endif
+
+#ifndef REND_FUNC
+#define REND_FUNC
+#define rend(vector) (vector)->functions->rend(vector)
+#endif
+
+//iterator
+#define iterator_vector(type) iterator_vector_##type
+
+//#define iter_vector_deref(it) it->iter->data
+
+#define iter_vector_equal(it1, it2) it1->iter == it2->iter
+#define iter_vector_notequal(it1, it2) it1->iter != it2->iter
+#define iter_vector_forward(it) it->iter = it->iter!=NULL ? (it->is_reverse ? it->iter-1 : it->iter+1) : NULL
+#define iter_vector_backward(it) it->iter = it->iter!=NULL ? (it->is_reverse ? it->iter+1 : it->iter-1) : NULL
 
 #endif
